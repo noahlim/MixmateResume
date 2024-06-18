c6b22a4bb5733005b00b0e5c081b1cc5f33c5148
harryGIbong <tnstndico@gmail.com>
2024-06-17 22:21:40 -0400

    .env fixed

--- mixmate/src/app/_utilities/_client/constants.ts
+++ mixmate/src/app/_utilities/_client/constants.ts
@@ -1,9 +1,9 @@
 import { AlertColor } from '@mui/material/Alert';
-
+import { endPoint, mixmate_domain } from '../_server/database/config';
 // const END_POINT = "https://mixmate-one.vercel.app/api";
 // const MIXMATE_DOMAIN = 'https://mixmate-one.vercel.app/';
-const END_POINT = "http://localhost:3000/api";
-const MIXMATE_DOMAIN = 'http://localhost:3000/';
+const END_POINT = endPoint;
+const MIXMATE_DOMAIN = mixmate_domain;
 
 const SEVERITY: Record<string, AlertColor> = {
   Info: 'info',
